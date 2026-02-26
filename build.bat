9c49079d7e3aa98aa8720cd6330cfb2535e34978
TrueHopolok <vadim.truhanov@inbox.lv>
2026-02-26 11:40:26 +0200

    fixup! updated engine to be an oop object and not a set of static functions

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-g++ example_util_2.cpp AllegroUtil.cpp -o bin/program -std=c++17 -Iallegro/include -Lallegro/lib -lallegro -lallegro_main -lallegro_primitives
+g++ example_engine_3.cpp engine.cpp -o bin/program -std=c++17 -Iallegro/include -Lallegro/lib -lallegro -lallegro_main -lallegro_primitives
